471a29f8a4091c02df928da85f3e41bc17a5334c
emilio <feedingaliencat@gmail.com>
2014-07-01 11:38:48 +0200

    Flask added, but there is a metaclass conflict

--- indiserver.py
+++ indiserver.py
@@ -5,12 +5,30 @@
 from credentials import _DATABASE_URL
 
 # library imports
-from sqlalchemy import create_engine, relationship, \
+from flask import Flask, request
+from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.restful import abort, Api, Resource
+from sqlalchemy import create_engine, \
         Column, ForeignKey, \
         String, Integer, Float, Text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy_imageattach.entity import Image, image_attachment
+
+# HTTP service codes
+HTTP_OK = 200
+HTTP_CREATED = 201
+HTTP_BAD_REQUEST = 400
+HTTP_NOT_FOUND = 404
+HTTP_CONFLICT = 409
+
+# api prefix, common in all the requests
+API_PREFIX = "/api/v1/"
+
+# Flask setup
+app = Flask(__name__)
+api = Api(app)
+auth = HTTPBasicAuth()
 
 # access to the database
 Base = declarative_base()
@@ -18,8 +36,8 @@ engine = create_engine(_DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
 
 
-# database classes
-class User(Base):
+# database and Flask RESTful classes
+class User(Resource, Base):
     __tablename__ = "users"
 
     name = Column(String(50), primary_key=True)
@@ -33,8 +51,26 @@ class User(Base):
             self.psw
         )
 
+    def post(self):
+        """
+        Create a new user
+        Expected as POST data:
+        { "user": <username>, "psw": <password> }
+        """
+        try:
+            user = request.form["user"]
+            psw = request.form["psw"]
+        except KeyError:
+            abort(HTTP_BAD_REQUEST)
 
-class OpenData(Base):
+        # TODO: controlla che il nome non sia già preso
+        # TODO: crea l'utente
+
+
+class OpenData(Base, Resource):
+    # radius of data scanning
+    RADIUS = 300        # TODO: settarlo a modino
+
     __tablename__ = "opendata"
 
     lat = Column(Float, primary_key=True)
@@ -50,8 +86,25 @@ class OpenData(Base):
             self.name,
         )
 
+    def get(self):
+        '''
+        Expected lat and lon as GET arguments, floating point.
+        Return the list of POI in the defined radius.
+        '''
+        try:
+            lat = request.args.get("lat")
+            lon = request.args.get("lon")
+        except KeyError:
+            abort(HTTP_BAD_REQUEST)
+        # TODO: controllo validità
+        to_return = []
+        for p in OpenData:      # ?? tenere in memoria la lista degli open data..
+            if math.sqrt((lat-p.lat)**2 + (lon-p.lon)**2) < OpenData.RADIUS:
+                to_return.append(p)
+        return to_return, HTTP_OK
 
-class CustomContent(Base):
+
+class CustomContent(Base, Resource):
     __tablename__ = "usercontents"
 
     id_ = Column(Integer, primary_key=True)
@@ -63,6 +116,59 @@ class CustomContent(Base):
     ForeignKeyConstraint(["lat", "lon"], ["OpenData.lat", "OpenData.lon"])
     # TODO: check that any comment or picture are present
     likes = relationship("Like")
+
+    @auth.login_required
+    def post(self):
+        '''
+        Upload a new CustomContent. It has to be a photo and/or a comment,
+        only logged in users can use it,
+        '''
+        user = auth.username()
+
+        try:
+            lat = request.form["lat"]
+            lon = request.form["lon"]
+        except KeyError:
+            abort(HTTP_BAD_REQUEST)
+
+        if ("comment" in request.form) or ("photo" in request.form):    # TODO: foto?!
+            try:
+                comment = request.form["comment"]
+                photo = request.form["photo"]
+            except KeyError:
+                # there is only a comment or a photo
+                pass
+        else:
+            abort(HTTP_BAD_REQUEST)
+
+        # TODO: crea l'oggetto
+
+    def get(self):
+        '''
+        Get a list of first 10 contents, ordered by likes,
+        starting from the integer "page" (0, 1, 2...)
+        '''
+        try:
+            lat = request.form["lat"]
+            lon = request.form["lon"]
+            page = request.form["page"]
+        except KeyError:
+            abort(HTTP_BAD_REQUEST)
+
+        # TODO: ritorna l'oggetto
+    
+    @auth.login_required
+    def delete(self):
+        '''
+        Let to an user to delete a his own content (by id_).
+        '''
+        try:
+            id_ = request.form["id"]
+        except KeyError:
+            abort(HTTP_BAD_REQUEST)
+
+        # TODO: if contenuto[id_].user == auth.username():
+        # TODO:     cancella l'oggetto
 
 
 class Photo(Base, Image):
@@ -76,12 +182,35 @@ class Photo(Base, Image):
     user = relationship("CustomContent")
 
 
-class Like(Base):
-    user = (String(50), ForeignKey("User.name"), primary_key=True)
+class Like(Base, Resource):
+    user = Column(String(50), ForeignKey("User.name"), primary_key=True)
     id_ = Column(Integer, ForeignKey("CustomContent.id_"), primary_key=True)
     
     # True is a like, False is an unlike
     vote = Column(Boolean, nullable=False)
 
+    @auth.login_required
+    def get(self):
+        try:
+            id_ = request.get.args["id_"]
+        except KeyError:
+            abort(HTTP_BAD_REQUEST)
+
+        # TODO: cancella l'oggetto
+
+
+@auth.verify_password
+def verify_password(username, password):
+    # TODO: controlla che la coppia username e password sia nel db
+    return True
+
+
+api.add_resource(User, "{}create_user".format(API_PREFIX))
+api.add_resource(OpenData, "{}infos_from_map".format(API_PREFIX))
+api.add_resource(CustomContent, "{}contents".format(API_PREFIX))
+api.add_resource(Like, "{}like".format(API_PREFIX))
+
+
 if __name__ == "__main__":
-	pass
+    # TODO: carica il database
+    app.run(host="0.0.0.0", debug=True)         # TODO: remove debug=True
