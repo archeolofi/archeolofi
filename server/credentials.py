df785c4732db22fac1bd025ff88ef0dc55b1a464
emilio <feedingaliencat@gmail.com>
2014-08-01 17:17:24 +0200

    added credentials.pyt (modify them before deploygit status)

---
+++ server/credentials.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+
+db = {
+	"username" : "indiana",
+	"password" : "astropanza",
+	"host"     : "localhost",
+	"database" : "indiana",
+}
+
+
+DATABASE_URL = ''.join([
+    "postgresql://",
+    db["username"],
+    ":",
+    db["password"],
+    "@",
+    db["host"],
+    "/",
+    db["database"]
+])
