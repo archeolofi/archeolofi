0401704fd8e56d1cffaa9db2436cf5be2fcfc8cb
emilio <feedingaliencat@gmail.com>
2014-08-12 16:33:09 +0200

    ajax cross_domain. access control allow origin decorator (but I can't use it with restless methods)

--- server/indiserver.py
+++ server/indiserver.py
@@ -2,14 +2,18 @@
 #-*- coding: utf-8 -*-
 
 # library imports
-from flask import Flask, request, abort
+from flask import Flask, request, abort, current_app
 import flask.ext.sqlalchemy
 import flask.ext.restless as restless
 # from sqlalchemy_imageattach.entity import Image, image_attachment
 import json
+from functools import wraps
 
 # local imports
 from credentials import DATABASE_URL
+
+# TODO: remove
+from pdb import set_trace
 
 #in Openshift will be:
 #DATABASE_URL = "postgresql://$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT"
@@ -25,6 +29,62 @@ HTTP_CONFLICT = 409
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
+
+
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    """
+    http://flask.pocoo.org/snippets/56/
+    """
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
+@app.route('/api/cross_domain')
+@crossdomain(origin='*')
+def my_service():
+    return "I'm cool. Every domain is cool"
 
 
 # database and Flask classes (RESTless)
@@ -238,7 +298,10 @@ manager = restless.APIManager(app, flask_sqlalchemy_db=db)
 
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well
-manager.create_api(IndianaUser, methods=["POST"])
+manager.create_api(
+    IndianaUser,
+    methods=["POST"]
+)
 manager.create_api(
     Content,
     methods=["GET", "POST"],
