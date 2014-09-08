f17202b98e9ffc4e3ca3285a1e2f3e9373ae231f
emilio <feedingaliencat@gmail.com>
2014-09-08 21:36:58 +0200

    "download" metatag

--- client/functions.js
+++ client/functions.js
@@ -186,7 +186,7 @@ function setting_info(data){
 function file_thumb(entry) {
     // TODO: correggere in 'data:image/jpeg;base64,' + entry["photo_thumb"];
     var thumb = (
-            '<a href="' + SERVER_URL + 'contents/' + entry["filename"] + '">'
+            '<a href="' + SERVER_URL + 'contents/' + entry["filename"] + '" download>'
         +   '   <img src="'
         +   (entry["photo_thumb"] ? 'data:image;base64,' + entry["photo_thumb"] : FILE_ICON) + '" '
         +   '" />'
