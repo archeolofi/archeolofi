d6b57b39ea2b9e8a5168d38f2842b6283c33c26a
emilio <feedingaliencat@gmail.com>
2014-09-16 01:52:11 +0200

    big clean-up

--- progetto_archeolofi/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
--- progetto_archeolofi/platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes.dex : \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\archeolofi.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\BuildConfig.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\R$attr.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\R$drawable.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\R$string.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\R$xml.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\classes\com\progetto\archeolofi\R.class \
-C:\Users\marchetti\Documents\indiana\progetto_archeolofi\platforms\android\ant-build\dexedLibs\classes-157d65b2b943d2aa9bc7f3aa728d89df.jar \
