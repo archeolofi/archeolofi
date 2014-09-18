a3e8c54fa691ad747073100e097777e25983f89b
emilio <feedingaliencat@gmail.com>
2014-09-18 20:19:53 +0200

    follow up 064164dec94a0781652e18ab3628f213e1d6423f settings for server

---
+++ server/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='archeolofi',
+      version='0.1',
+      description='Firenze archeo app',
+      author='Francesco Marchetti, Emilio Veloci',
+      author_email='feedingaliencat@gmail.com',
+      url='not yet',
+#      install_requires=['Django>=1.3'],
+     )
