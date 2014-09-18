1878ed10891c0975b8040854ae6c4823081539e1
emilio <feedingaliencat@gmail.com>
2014-09-18 20:21:28 +0200

    in local works PIL and not Pillow

--- server/server.py
+++ server/server.py
@@ -7,7 +7,8 @@ import flask.ext.restless as restless
 from passlib.hash import sha256_crypt
 from base64 import b64encode
 import flask.ext.sqlalchemy
-from PIL import Image
+# from PIL import Image
+import Image
 import requests
 import json
 import time
