851f446843cbe77d1a1fef01ac09179193a71cb6
emilio <feedingaliencat@gmail.com>
2014-09-18 20:17:44 +0200

    client settings for openshift

--- client/js/location.js
+++ client/js/location.js
@@ -1,5 +1,3 @@
 
-// local
-var SERVER_URL = "http://127.0.0.1:5000/";
 // OpenShift
-//var SERVER_URL = "http://archeolofi-feedingaliencat.rhcloud.com/";
+var SERVER_URL = "http://archeolofi-feedingaliencat.rhcloud.com/";
