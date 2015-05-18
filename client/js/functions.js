b89cbef19b02660ced2fac117db20be626dbb6d1
Emilio Veloci <feedingaliencat@gmail.com>
2015-05-18 21:18:53 +0200

    Hide logout popup when user changes page

--- client/js/functions.js
+++ client/js/functions.js
@@ -417,6 +417,7 @@ function check_if_logged_in() {
 // pagechange
 // pagechangefailed
 $(document).on('pagebeforeshow', '#home', function() {
+    $(".logout_popup").hide();
     check_if_logged_in();
 });
 
